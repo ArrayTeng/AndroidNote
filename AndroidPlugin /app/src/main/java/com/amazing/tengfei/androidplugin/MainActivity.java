55d50a2fc80bda999f8c3045197741db1d086af0
tengfei <arrayadapter.cn@gmail.com>
2020-06-21 13:35:42 +0800

    uodate

---
+++ AndroidPlugin /app/src/main/java/com/amazing/tengfei/androidplugin/MainActivity.java
@@ -0,0 +1,19 @@
+package com.amazing.tengfei.androidplugin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.amazing.tengfei.androidplugin.hook.HookHelper;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        HookHelper.hookPackageManager(this);
+
+    }
+}
---
+++ AndroidPlugin /app/src/main/java/com/amazing/tengfei/androidplugin/utils/RefInvoke.java
@@ -0,0 +1,244 @@
+package com.amazing.tengfei.androidplugin.utils;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+/**
+ * @author 滕飞
+ * date 2020/6/20 9:55 PM
+ * email arrayadapter.cn@outlook.com
+ * description Java反射工具类
+ */
+@SuppressWarnings("rawtypes")
+public class RefInvoke {
+    //无参
+    public static Object createObject(String className) {
+        Class[] pareTypes = new Class[]{};
+        Object[] pareValues = new Object[]{};
+
+        try {
+            Class r = Class.forName(className);
+            return createObject(r, pareTypes, pareValues);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    //无参
+    public static Object createObject(Class clazz) {
+        Class[] pareTypes = new Class[]{};
+        Object[] pareValues = new Object[]{};
+
+        return createObject(clazz, pareTypes, pareValues);
+    }
+
+    //一个参数
+    public static Object createObject(String className, Class pareTyple, Object pareVaule) {
+        Class[] pareTypes = new Class[]{pareTyple};
+        Object[] pareValues = new Object[]{pareVaule};
+
+        try {
+            Class r = Class.forName(className);
+            return createObject(r, pareTypes, pareValues);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    //一个参数
+    public static Object createObject(Class clazz, Class pareTyple, Object pareVaule) {
+        Class[] pareTypes = new Class[]{pareTyple};
+        Object[] pareValues = new Object[]{pareVaule};
+
+        return createObject(clazz, pareTypes, pareValues);
+    }
+
+    //多个参数
+    public static Object createObject(String className, Class[] pareTyples, Object[] pareVaules) {
+        try {
+            Class r = Class.forName(className);
+            return createObject(r, pareTyples, pareVaules);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    //多个参数
+    public static Object createObject(Class clazz, Class[] pareTyples, Object[] pareVaules) {
+        try {
+            Constructor cor = clazz.getDeclaredConstructor(pareTyples);
+            cor.setAccessible(true);
+            return cor.newInstance(pareVaules);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+
+    //多个参数
+    public static Object invokeInstanceMethod(Object obj, String methodName, Class[] pareTypes, Object[] pareValues) {
+        if (obj == null)
+            return null;
+
+        try {
+            //调用一个private方法
+            Method method = obj.getClass().getDeclaredMethod(methodName, pareTypes); //在指定类中获取指定的方法
+            method.setAccessible(true);
+            return method.invoke(obj, pareValues);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    //一个参数
+    public static Object invokeInstanceMethod(Object obj, String methodName, Class pareTyple, Object pareVaule) {
+        Class[] pareTypes = {pareTyple};
+        Object[] pareValues = {pareVaule};
+
+        return invokeInstanceMethod(obj, methodName, pareTypes, pareValues);
+    }
+
+    //无参
+    public static Object invokeInstanceMethod(Object obj, String methodName) {
+        Class[] pareTypes = new Class[]{};
+        Object[] pareValues = new Object[]{};
+
+        return invokeInstanceMethod(obj, methodName, pareTypes, pareValues);
+    }
+
+    //无参
+    public static Object invokeStaticMethod(String className, String method_name) {
+        Class[] pareTypes = new Class[]{};
+        Object[] pareValues = new Object[]{};
+
+        return invokeStaticMethod(className, method_name, pareTypes, pareValues);
+    }
+
+    //一个参数
+    public static Object invokeStaticMethod(String className, String method_name, Class pareTyple, Object pareVaule) {
+        Class[] pareTyples = new Class[]{pareTyple};
+        Object[] pareVaules = new Object[]{pareVaule};
+
+        return invokeStaticMethod(className, method_name, pareTyples, pareVaules);
+    }
+
+    //多个参数
+    public static Object invokeStaticMethod(String className, String method_name, Class[] pareTyples, Object[] pareVaules) {
+        try {
+            Class obj_class = Class.forName(className);
+            return invokeStaticMethod(obj_class, method_name, pareTyples, pareVaules);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    //无参
+    public static Object invokeStaticMethod(Class clazz, String method_name) {
+        Class[] pareTypes = new Class[]{};
+        Object[] pareValues = new Object[]{};
+
+        return invokeStaticMethod(clazz, method_name, pareTypes, pareValues);
+    }
+
+    //一个参数
+    public static Object invokeStaticMethod(Class clazz, String method_name, Class classType, Object pareVaule) {
+        Class[] classTypes = new Class[]{classType};
+        Object[] pareValues = new Object[]{pareVaule};
+
+        return invokeStaticMethod(clazz, method_name, classTypes, pareValues);
+    }
+
+    //多个参数
+    public static Object invokeStaticMethod(Class clazz, String method_name, Class[] pareTyples, Object[] pareVaules) {
+        try {
+            Method method = clazz.getDeclaredMethod(method_name, pareTyples);
+            method.setAccessible(true);
+            return method.invoke(null, pareVaules);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    //简写版本
+    public static Object getFieldObject(Object obj, String filedName) {
+        return getFieldObject(obj.getClass(), obj, filedName);
+    }
+
+    public static Object getFieldObject(String className, Object obj, String filedName) {
+        try {
+            Class obj_class = Class.forName(className);
+            return getFieldObject(obj_class, obj, filedName);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static Object getFieldObject(Class clazz, Object obj, String filedName) {
+        try {
+            Field field = clazz.getDeclaredField(filedName);
+            field.setAccessible(true);
+            return field.get(obj);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    //简写版本
+    public static void setFieldObject(Object obj, String filedName, Object filedVaule) {
+        setFieldObject(obj.getClass(), obj, filedName, filedVaule);
+    }
+
+    public static void setFieldObject(Class clazz, Object obj, String filedName, Object filedVaule) {
+        try {
+            Field field = clazz.getDeclaredField(filedName);
+            field.setAccessible(true);
+            field.set(obj, filedVaule);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void setFieldObject(String className, Object obj, String filedName, Object filedVaule) {
+        try {
+            Class obj_class = Class.forName(className);
+            setFieldObject(obj_class, obj, filedName, filedVaule);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Object getStaticFieldObject(String className, String filedName) {
+        return getFieldObject(className, null, filedName);
+    }
+
+    public static Object getStaticFieldObject(Class clazz, String filedName) {
+        return getFieldObject(clazz, null, filedName);
+    }
+
+    public static void setStaticFieldObject(String classname, String filedName, Object filedVaule) {
+        setFieldObject(classname, null, filedName, filedVaule);
+    }
+
+    public static void setStaticFieldObject(Class clazz, String filedName, Object filedVaule) {
+        setFieldObject(clazz, null, filedName, filedVaule);
+    }
+}
