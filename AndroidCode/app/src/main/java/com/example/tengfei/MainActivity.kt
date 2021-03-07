14c6baf99a3d6b0b57e70fe0987b748935b41344
tengfei <arrayadapter.cn@gmail.com>
2021-03-07 21:47:41 +0800

    init project

---
+++ AndroidCode/app/src/main/java/com/example/tengfei/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.tengfei
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dalvik.system.BaseDexClassLoader
+import dalvik.system.DexClassLoader
+import dalvik.system.PathClassLoader
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
