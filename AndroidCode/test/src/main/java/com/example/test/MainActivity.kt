274c3203b7e050ed974044ecc054791e42c5a9fc
tengfei <arrayadapter.cn@gmail.com>
2021-03-11 22:30:19 +0800

    update

---
+++ AndroidCode/test/src/main/java/com/example/test/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import io.reactivex.Observable
+import io.reactivex.ObservableEmitter
+import io.reactivex.ObservableOnSubscribe
+import io.reactivex.Observer
+import io.reactivex.disposables.Disposable
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+      Observable.create(object :ObservableOnSubscribe<String>{
+          override fun subscribe(e: ObservableEmitter<String>) {
+              TODO("Not yet implemented")
+          }
+
+      }).subscribe(object:Observer<String>{
+          override fun onComplete() {
+              TODO("Not yet implemented")
+          }
+
+          override fun onSubscribe(d: Disposable) {
+              TODO("Not yet implemented")
+          }
+
+          override fun onNext(t: String) {
+              TODO("Not yet implemented")
+          }
+
+          override fun onError(e: Throwable) {
+              TODO("Not yet implemented")
+          }
+      })
+
+
+    }
+}
+
+
+
---
+++ AndroidCode/test/src/main/java/com/example/test/Test.java
@@ -0,0 +1,40 @@
+package com.example.test;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.Disposable;
+
+public class Test {
+
+    void  test(){
+        Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(@NonNull ObservableEmitter<String> e) throws Exception {
+                e.
+            }
+        }).subscribe(new Observer<String>() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(@NonNull String s) {
+
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
+    }
+}
