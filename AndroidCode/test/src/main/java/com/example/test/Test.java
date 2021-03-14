5569d40ee67506fd9a41679ce27f91c15f3d8348
tengfei <arrayadapter.cn@gmail.com>
2021-03-14 22:31:10 +0800

    看了一会代码，但没看下去

--- AndroidCode/test/src/main/java/com/example/test/Test.java
+++ AndroidCode/test/src/main/java/com/example/test/Test.java
@@ -4,42 +4,43 @@ import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.annotations.NonNull;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class Test {
 
 
+    void test() {
+
+        Observable.create(new ObservableOnSubscribe<String>() {
+            public void subscribe(@NonNull ObservableEmitter<String> e) throws Exception {
+
+            }
+        }).subscribeOn(Schedulers.io())//ObservableCreate执行subscribeOn，将source替换为了ObservableCreate 返回 ObservableSubscribeOn
+                //ObservableSubscribeOn.observeOn，将source替换为了 ObservableSubscribeOn  返回 ObservableObserveOn
+                .observeOn(AndroidSchedulers.mainThread())
+                //ObservableObserveOn.subscribe
+                .subscribe(new Observer<String>() {//最终这里执行的是 ObservableObserveOn 的subscribe函数也就是 ObservableObserveOn 的 subscribeActual 函数
+                    public void onSubscribe(@NonNull Disposable d) {
+
+                    }
+
+                    public void onNext(@NonNull String s) {
+
+                    }
+
+                    public void onError(@NonNull Throwable e) {
+
+                    }
+
+                    public void onComplete() {
+
+                    }
+                });
 
 
-
-
-
-
-
-
-
-    void  test(){
-       Observable.create(new ObservableOnSubscribe<String>() {
-           public void subscribe(@NonNull ObservableEmitter<String> e) throws Exception {
-
-           }
-       }).subscribe(new Observer<String>() {
-           public void onSubscribe(@NonNull Disposable d) {
-
-           }
-
-           public void onNext(@NonNull String s) {
-
-           }
-
-           public void onError(@NonNull Throwable e) {
-
-           }
-
-           public void onComplete() {
-
-           }
-       });
     }
 }
