dad41491e7f34e14d8bf34cf0567ab1a881d3c8d
ArrayTeng <arrayadapter.cn@gmail.com>
2021-03-16 16:52:41 +0800

    ClassLoader

--- AndroidCode/multidex/src/main/java/com/example/multidex/ShareReflectUtil.java
+++ AndroidCode/multidex/src/main/java/com/example/multidex/ShareReflectUtil.java
@@ -1,22 +1,97 @@
 package com.example.multidex;
 
+import java.lang.reflect.Array;
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class ShareReflectUtil {
 
+
+    /**
+     * 从 instance 到其父类 找 name 属性
+     * @param instance
+     * @param name
+     * @return
+     * @throws NoSuchFieldException
+     */
     public static Field findField(Object instance, String name) throws NoSuchFieldException {
         for (Class<?> clazz = instance.getClass(); clazz != null; clazz = clazz.getSuperclass()) {
             try {
+                //查找当前类的 属性(不包括父类)
                 Field field = clazz.getDeclaredField(name);
+
                 if (!field.isAccessible()) {
                     field.setAccessible(true);
                 }
                 return field;
             } catch (NoSuchFieldException e) {
-                e.printStackTrace();
+                // ignore and search next
             }
         }
-        throw new NoSuchFieldException("Field" + name + " not found in " + instance.getClass());
+        throw new NoSuchFieldException("Field " + name + " not found in " + instance.getClass());
+    }
+
+    /**
+     * 从 instance 到其父类 找  name 方法
+     * @param instance
+     * @param name
+     * @return
+     * @throws NoSuchFieldException
+     */
+    public static Method findMethod(Object instance, String name, Class<?>... parameterTypes)
+            throws NoSuchMethodException {
+        for (Class<?> clazz = instance.getClass(); clazz != null; clazz = clazz.getSuperclass()) {
+            try {
+                Method method = clazz.getDeclaredMethod(name, parameterTypes);
+
+                if (!method.isAccessible()) {
+                    method.setAccessible(true);
+                }
+
+                return method;
+            } catch (NoSuchMethodException e) {
+                // ignore and search next
+            }
+        }
+        throw new NoSuchMethodException("Method "
+                + name
+                + " with parameters "
+                + Arrays.asList(parameterTypes)
+                + " not found in " + instance.getClass());
+    }
+
+
+    /**
+     *
+     * @param instance
+     * @param fieldName
+     * @param extraElements 补丁的Element数组 其实就1个
+     * @throws NoSuchFieldException
+     * @throws IllegalArgumentException
+     * @throws IllegalAccessException
+     */
+    public static void expandFieldArray(Object instance, String fieldName, Object[] extraElements)
+            throws NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
+        //拿到 classloader中的dexelements 数组
+        Field jlrField = findField(instance, fieldName);
+        Object[] original = (Object[]) jlrField.get(instance); //Element
+
+        //把 extraElements+original 合并到一起
+
+        //合并后的数组
+        Object[] combined = (Object[]) Array.newInstance(original.getClass().getComponentType(), original.length + extraElements.length);
+
+        // NOTE: changed to copy extraElements first, for patch load first
+        // 先拷贝新数组
+        System.arraycopy(extraElements, 0, combined, 0, extraElements.length);
+        System.arraycopy(original, 0, combined, extraElements.length, original.length);
+
+        //修改 classLoader中的 dexelements的 值
+        jlrField.set(instance, combined);
+
+
+
     }
 }
 
