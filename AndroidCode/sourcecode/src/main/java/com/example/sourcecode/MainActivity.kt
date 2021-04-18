b8f83bbea76b0c37b2e6448c8c00a3afed8a83e9
滕飞 <arrayadapter.cn@gmail.com>
2021-04-18 19:10:05 +0800

    添加了 9.0 的源码

--- AndroidCode/sourcecode/src/main/java/com/example/sourcecode/MainActivity.kt
+++ AndroidCode/sourcecode/src/main/java/com/example/sourcecode/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.sourcecode
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var threadLocal :ThreadLocal<String>
+
+    lateinit var handler: Handler
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        threadLocal.remove()
+
+        handler.sendMessage()
     }
 }
